3ad0a39a2585f5d629468457ba218780267e990e
joaolavelino <joaolavelino@gmail.com>
2022-04-25 13:32:04 -0300

    AULA 5 - Middlewares

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const { process_params } = require("express/lib/router");
 
 const app = express();
 //todos os métodos do servidor estarão dentro de app
+
+app.use(express.json());
+//esse middleware verifica se tem um JSON disponível no body da requisiçao
+
+//criar um middleware
+const log = (req, res, next) => {
+  console.log(req.body);
+  console.log(`Requested on: ${Date.now()}`);
+  next();
+  //next = passa pro prox middleware
+};
+app.use(log);
 
 //first request routes
 app.get("/", (req, res) => {
