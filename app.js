2fe8d4c0ce1f4d2a5ddde548e22a80c4ac6837ef
joaolavelino <joaolavelino@gmail.com>
2022-04-25 12:55:54 -0300

    AULA 2 - Primeira rota com Express + Nodemon

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const express = require("express");
+
+const app = express();
+//todos os métodos do servidor estarão dentro de app
+
+//first request routes
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>My to-do-list</h1><h2>Created with NodeJS + Express + MongoDB<h2/>"
+  );
+});
+
+//set the port for the server
+app.listen(3000, () => {
+  console.log("The server is now started");
+});
