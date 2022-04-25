0945af8eaec1145dd93d7daa3edc7d915b541c26
joaolavelino <joaolavelino@gmail.com>
2022-04-25 17:36:51 -0300

    AULA 13 - Rotas Put e Delete

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -34,14 +34,29 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
-  console.log(req.params.id);
-  res.send(`PUT ID: ${req.params.id}`);
+router.put("/:id", async (req, res) => {
+  let { name } = req.body;
+  try {
+    let checklist = await Checklist.findByIdAndUpdate(
+      req.params.id,
+      { name },
+      { new: true }
+    );
+    console.log(`The checklist was edited\nthe new name is: ${checklist.name}`);
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
 });
 
-router.delete("/:id", (req, res) => {
-  console.log(req.params.id);
-  res.send(`DELETE ID: ${req.params.id}`);
+router.delete("/:id", async (req, res) => {
+  try {
+    let checklist = await Checklist.findByIdAndRemove(req.params.id);
+    console.log(`${checklist.name} was removed`);
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
